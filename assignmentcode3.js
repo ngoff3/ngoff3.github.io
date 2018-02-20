516e4ebf02bf9271599efd436ff09b7596fc9568
ngoff3 <35709800+ngoff3@users.noreply.github.com>
2018-02-19 19:03:05 -0600

    Codeassignment3

---
+++ assignmentcode3.js
@@ -0,0 +1,10 @@
+let project = 1
+let projectTitle = "Map 1"
+let projectUrl = "https://example.com"
+let hasThumbnail = true
+let projectKeywords = ['web gis', 'earthquakes']
+
+console.log(project)
+console.log(projectTitle)
+console.log(projectUrl)
+console.log(hasThumbnail)
