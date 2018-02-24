fd4595e84fbb01034b10abb6ffdba25237f28ca4
ngoff3 <35709800+ngoff3@users.noreply.github.com>
2018-02-23 19:53:45 -0600

    Updated changes

--- assignmentcode3.js
+++ assignmentcode3.js
@@ -4,7 +4,8 @@ let projectUrl = "https://example.com"
 let hasThumbnail = true
 let projectKeywords = ['web gis', 'earthquakes']
 
-console.log(project)
-console.log(projectTitle)
-console.log(projectUrl)
-console.log(hasThumbnail)
+console.log('ID: ' + project)
+console.log('Project Title: ' + projectTitle)
+console.log('URL: ' + projectUrl)
+console.log('Has thumbnail? ' + hasThumbnail)
+console.log('Keywords: ' + projectKeywords)
