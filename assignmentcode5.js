9d56b68a0d2409d8260670462bf7794dcaf4f0de
ngoff3 <35709800+ngoff3@users.noreply.github.com>
2018-03-08 18:37:35 -0600

    h

---
+++ assignmentcode5.js
@@ -0,0 +1,23 @@
+let projectData = []
+
+  projectData[0] = {
+    id: 1,
+    title: 'Book Assignment 1',
+    url: 'http://arcg.is/2n2jqWJ',
+    hasThumbnail: true
+  }
+
+  projectData[1] = {
+    id: 2,
+    title: 'Book Assignment 2',
+    url: 'http://www.arcgis.com/apps/View/index.html?appid=e7c1a0258eed4a82828ca53a980852a4',
+    hasThumbnail: false
+  }
+
+for (let i = 0; i < projectData.length; i++) {
+  if (projectData[i].hasThumbnail === true) {
+    console.log('images/ss-project-' + projectData[i].id + '.png')
+  } else {
+    console.log('no-preview.png')
+  }
+}
