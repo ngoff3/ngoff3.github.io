45039f20fe2b7c6cb6ef05facc93f54963703d88
ngoff3 <35709800+ngoff3@users.noreply.github.com>
2018-04-02 16:46:07 -0500

    1

---
+++ map/webmap-assignment-03.js
@@ -0,0 +1,38 @@
+let mymap = L.map('natesmapid3').setView([32.804739, -90.593429], 11)
+let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+L.tileLayer(basemapUrl).addTo(mymap)
+let poi = L.marker([32.795504,	-90.571457]).addTo(mymap)
+let polygon = L.polygon([
+  [32.892421,	-90.656601],
+  [32.887808,	-90.509659],
+  [32.723902,	-90.597549]
+]).addTo(mymap)
+
+var myGreenIcon = L.icon({
+  iconUrl: 'leaf-green.png',
+  shadowUrl: 'leaf-shadow.png',
+  iconSize:     [38, 95], // size of the icon
+  shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+})
+
+let markerCoords = [32.795504,	-90.571457]
+let markerOptions = { icon: myGreenIcon}
+let myMarker = L.marker(markerCoords, markerOptions).addTo(mymap)
+
+poi.bindPopup('Panther Swamp')
+polygon.bindPopup('Mississippi Wildlife Refuge')
+
+function logCurrentCoordinates (event) {
+  console.log('You clicked the map at ' + event.latlng)
+}
+mymap.on('click', logCurrentCoordinates)
+
+// create a red polyline from an array of LatLng points
+let latlngs = [
+  [32.816281,	-90.675827],
+  [32.822051,	-90.493179],
+  ];
+let polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
